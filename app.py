31920c1a71e0dfe358d7b9078f710045f3bac283
Wallisson Victor <86571646+PilzTol@users.noreply.github.com>
2023-12-22 00:00:40 +0000

    A3.4

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 #import yfinance as yf
-
+from plot.interactive import plot_line_i
 
 st.title('Stock Price App')
 #Sidebar 
 st.sidebar.header('User Input')
 symbol = st.sidebar.text_input('Esolha um ativo:', 'AAPL')
 st.write(symbol)
+
+#Plot
+
+fig = plot_line_i(symbol)
+st.plotly_chart(fig)
